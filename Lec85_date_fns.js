dbf934fb65b5032c4344d9b2c7bec3f8809c5356
Naomi Kapele <naomikapele@booknowzambia.com>
2024-11-25 18:04:23 +0200

    second commit

--- Lec85_date_fns.js
+++ Lec85_date_fns.js
@@ -3,9 +3,15 @@
 const now = new Date();
 
 console.log(dateFns.isToday(now));
-console.log(dateFns.format(now, 'YYYY'));
-console.log(dateFns.format(now, 'MMM'));
-console.log(dateFns.format(now, 'dddd'));
-console.log(dateFns.format(now, 'Do'));
-console.log(dateFns.format(now, 'YYYY'));
+console.log(dateFns.format(now, "YYYY"));
+console.log(dateFns.format(now, "MMM"));
+console.log(dateFns.format(now, "dddd"));
+console.log(dateFns.format(now, "Do"));
+console.log(dateFns.format(now, "YYYY"));
 // console.log(dateFns.format(now, ' '));
+
+// important - e.g date user signed up, blog post was created
+// comparing dates using dateFns
+const before = new Date("February 1 2024 12:00:00");
+
+console.log(dateFns.distanceInWords(now, before, {addSuffix: true}));
